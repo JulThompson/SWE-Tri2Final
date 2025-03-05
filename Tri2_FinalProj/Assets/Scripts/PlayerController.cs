3c9d038022cf63875f276460dfd81b806fca710a
Julia <jewelst2014@gmail.com>
2025-03-04 19:57:37 -0500

    added player controller

---
+++ Tri2_FinalProj/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private PlayerState state;
+    public Rigidbody2D rb;
+    public int borderLeft;
+    public int borderRight;
+    public int velocity;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        state = new PlayerStateNormal(this, rb);
+        borderLeft = -10;
+        borderRight = 10;
+        velocity = 4;
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("right"))
+        {
+            rb.velocity = new Vector2(velocity, 0);
+        }
+        if (Input.GetKeyDown("left"))
+        {
+            rb.velocity = new Vector2(-velocity, 0);
+        }
+        if (!(Input.GetKeyDown("left")) && Input.GetKeyUp("right"))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+        if (!(Input.GetKeyDown("right")) && Input.GetKeyUp("left"))
+        {
+            rb.velocity = new Vector2(0, 0);
+        }
+    }
+
+    public void setState(PlayerState s)
+    {
+        state = s;
+    }
+}
